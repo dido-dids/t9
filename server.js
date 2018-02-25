72dd670f2568067bb15b07c29d62d3f246629bb1
David Boron <malyborky@gmail.com>
2018-02-25 18:25:50 +0100

    refactor

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-console.log(convertNumberToWords());
-
 app.get('/api/t9/:numbers', (req, res) => {
     res.json(convertNumberToWords(req.params.numbers));
 });
